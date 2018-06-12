5d1b40bf08713999356507344f751ba907138219
Winston Lim <winstonlim.2000@gmail.com>
2018-06-12 17:27:15 +0800

    Winston try 1

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         try {
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("LoginPage.fxml"));
-            System.out.println(getClass().getResource("LoginPage.fxml"));
+            loader.setLocation(getClass().getResource("HomeScreen.fxml"));
+            System.out.println(getClass().getResource("HomeScreen.fxml"));
             Parent root = loader.load();
             Scene scene = new Scene(root, 1067, 600);
             primaryStage.setResizable(false);
---
+++ src/SideTabController.java
@@ -0,0 +1,43 @@
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SideTabController {
+
+    @FXML
+    private JFXHamburger hamburger;
+
+    @FXML
+    private JFXDrawer drawer;
+
+    @FXML
+    private JFXButton button1;
+
+    @FXML
+    private JFXButton button2;
+
+    @FXML
+    private JFXButton button3;
+
+    @FXML
+    private JFXButton button4;
+
+    @FXML
+    private JFXButton button5;
+
+    @FXML
+    private JFXButton button6;
+
+    @FXML
+    private JFXButton button7;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
