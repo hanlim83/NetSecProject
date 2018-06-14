3afe4ece0cad79157f19936e98c4a5a9d7ead362
Kool-Koder <39305371+Kool-Koder@users.noreply.github.com>
2018-06-14 13:42:41 +0800

    Urgent BaseLayout format fix
    Fixed BaseLayout SideTab GUI bug.
    Affected fxml files include BaseLayoutNew and SideTab.
    
    Please use the new dimensions. Thank you

---
+++ src/ControllerUserSideTab.java
@@ -0,0 +1,36 @@
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ControllerUserSideTab {
+
+    @FXML
+    private JFXButton testButton;
+
+    private Scene myScene;
+
+    @FXML
+    void onClickTestButton(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("BaseLayout.fxml"));
+        System.out.println(getClass().getResource("BaseLayout.fxml"));
+        myScene = (Scene) ((Node) event.getSource()).getScene();
+        Stage stage = (Stage) (myScene).getWindow();
+        Parent nextView = loader.load();
+
+
+        ControllerBaseLayout controller = loader.<ControllerBaseLayout>getController();
+        //controller.passData(admin);
+
+        stage.setScene(new Scene(nextView));
+        stage.setTitle("NSPJ");
+        stage.show();
+    }
+}
