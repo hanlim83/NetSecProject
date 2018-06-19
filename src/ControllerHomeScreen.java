9e4df81fe6f0a965a4f389d9ef931b389db3b0be
Winston Lim <winstonlim.2000@gmail.com>
2018-06-19 19:43:52 +0800

    Created StorageSnippets.java
    Shared class to get buckets, create buckets, delete buckets, etc

---
+++ src/ControllerHomeScreen.java
@@ -0,0 +1,68 @@
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ControllerHomeScreen implements Initializable {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private JFXHamburger hamburger;
+
+    @FXML
+    private JFXDrawer drawer;
+
+    private Scene myScene;
+
+    public static AnchorPane rootP;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        hamburgerBar();
+    }
+
+
+    public void hamburgerBar() {
+        rootP = anchorPane;
+
+        try {
+            VBox box = FXMLLoader.load(getClass().getResource("SideTab.fxml"));
+            drawer.setSidePane(box);
+            drawer.setVisible(false);
+        } catch (IOException ex) {
+            Logger.getLogger(ControllerBaseLayoutNew.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(hamburger);
+        transition.setRate(-1);
+        hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, (e) -> {
+            System.out.println("Clicked");
+            transition.setRate(transition.getRate() * -1);
+            transition.play();
+
+            if (drawer.isOpened()) {
+                drawer.close();
+                drawer.setDisable(true);
+                //drawer.setVisible(false);
+            } else {
+                drawer.open();
+                drawer.setVisible(true);
+                drawer.setDisable(false);
+            }
+        });
+    }
+}
