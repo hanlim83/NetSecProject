5d0ef3a7cb0e9a8a3873df9eb5cde518d5f1d464
Kool-Koder <39305371+Kool-Koder@users.noreply.github.com>
2018-06-22 22:43:41 +0800

    Added AdminSideTab

---
+++ src/ControllerAdminSideTab.java
@@ -0,0 +1,37 @@
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ControllerAdminSideTab {
+
+
+    private Scene myScene;
+
+    @FXML
+    private JFXButton listCreate;
+
+    @FXML
+    void onClickBuckets(MouseEvent event) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("BucketsPage.fxml"));
+        myScene = (Scene) ((Node) event.getSource()).getScene();
+        Stage stage = (Stage) (myScene).getWindow();
+        Parent nextView = null;
+        try {
+            nextView = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        stage.setScene(new Scene(nextView));
+        stage.setTitle("Buckets Page");
+        stage.show();
+    }
+}
