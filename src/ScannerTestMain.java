1d8bd5f84eb91c6b33abd49953caacc82c3f6707
Ravin <170552L@mymail.nyp.edu.sg>
2018-06-25 09:16:44 +0800

    FIle Scanner enhancement
    File scanner edited and added more details.

--- src/ScannerTestMain.java
+++ src/ScannerTestMain.java
@@ -8,68 +8,62 @@ import java.io.File;
 import java.io.UnsupportedEncodingException;
 import com.kanishka.virustotal.dto.FileScanReport;
 import com.kanishka.virustotal.dto.VirusScanInfo;
-import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
 import java.util.Map;
 
 
 public class ScannerTestMain {
 
     public static void main(String[] args) {
+
+
         try {
             VirusTotalConfig.getConfigInstance().setVirusTotalAPIKey("1ac910b3fbfcb977b199e7113a20030386f81ce6ba242d4c56683789f08ae42e");
             VirustotalPublicV2 virusTotalRef = new VirustotalPublicV2Impl();
 
-            ScanInfo scanInformation = virusTotalRef.scanFile(new File("C:\\Users\\Fende\\Desktop\\eicar.com.txt"));
+            ScanInfo scanInformation = virusTotalRef.scanFile(new File("C:\\Users\\Fende\\Desktop\\test.txt"));
 
             System.out.println("___SCAN INFORMATION___");
-            /*System.out.println("MD5 :\t" + scanInformation.getMd5());
+            System.out.println("MD5 :\t" + scanInformation.getMd5());
             System.out.println("Perma Link :\t" + scanInformation.getPermalink());
             System.out.println("Resource :\t" + scanInformation.getResource());
             System.out.println("Scan Date :\t" + scanInformation.getScanDate());
             System.out.println("Scan Id :\t" + scanInformation.getScanId());
             System.out.println("SHA1 :\t" + scanInformation.getSha1());
-            System.out.println("SHA256 :\t" + scanInformation.getSha256());*/
+            System.out.println("SHA256 :\t" + scanInformation.getSha256());
             System.out.println("Verbose Msg :\t" + scanInformation.getVerboseMessage());
             System.out.println("Response Code :\t" + scanInformation.getResponseCode());
-            System.out.println("done.");
-        } catch (APIKeyNotFoundException ex) {
-            System.err.println("API Key not found! " + ex.getMessage());
-        } catch (UnsupportedEncodingException ex) {
-            System.err.println("Unsupported Encoding Format!" + ex.getMessage());
-        } catch (UnauthorizedAccessException ex) {
-            System.err.println("Invalid API Key " + ex.getMessage());
-        } catch (Exception ex) {
-            System.err.println("Something Bad Happened! " + ex.getMessage());
-        }
+            System.out.println("done.\n");
 
 
-        try {
-            VirusTotalConfig.getConfigInstance().setVirusTotalAPIKey("1ac910b3fbfcb977b199e7113a20030386f81ce6ba242d4c56683789f08ae42e");
-            VirustotalPublicV2 virusTotalRef = new VirustotalPublicV2Impl();
-
-            String resource="275a021bbfb6489e54d471899f7db9d1663fc695ec2fe2a2c4538aabf651fd0f";
+            String resource= scanInformation.getResource();
             FileScanReport report = virusTotalRef.getScanReport(resource);
 
-            System.out.println("MD5 :\t" + report.getMd5());
-            System.out.println("Perma link :\t" + report.getPermalink());
-            System.out.println("Resource :\t" + report.getResource());
-            System.out.println("Scan Date :\t" + report.getScanDate());
-            System.out.println("Scan Id :\t" + report.getScanId());
-            System.out.println("SHA1 :\t" + report.getSha1());
-            System.out.println("SHA256 :\t" + report.getSha256());
-            System.out.println("Verbose Msg :\t" + report.getVerboseMessage());
-            System.out.println("Response Code :\t" + report.getResponseCode());
+            int testPositive = report.getPositives();
+
             System.out.println("Positives :\t" + report.getPositives());
             System.out.println("Total :\t" + report.getTotal());
 
+
             Map<String, VirusScanInfo> scans = report.getScans();
+
             for (String key : scans.keySet()) {
                 VirusScanInfo virusInfo = scans.get(key);
                 System.out.println("Scanner : " + key);
                 System.out.println("\t\t Result : " + virusInfo.getResult());
                 System.out.println("\t\t Update : " + virusInfo.getUpdate());
-                System.out.println("\t\t Version :" + virusInfo.getVersion());
+                System.out.println("\t\t Version :" + virusInfo.getVersion() + "\n");
+            }
+
+            if (testPositive >= 5) {
+
+                System.out.println("\033[31;1mThis file is malicious!\033[0m");
+
+            }
+
+            else {
+
+                System.out.println("\033[32mThis file is safe!\033[0m");
+
             }
 
         } catch (APIKeyNotFoundException ex) {
@@ -81,6 +75,5 @@ public class ScannerTestMain {
         } catch (Exception ex) {
             System.err.println("Something Bad Happened! " + ex.getMessage());
         }
-
     }
 }
