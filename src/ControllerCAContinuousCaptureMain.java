0cb1aad026e55ae45c590a0bf68be24e46f85b30
Hansen Lim <hanlim83@users.noreply.github.com>
2018-07-04 00:42:45 +0800

    Added Missing FXML to Continuous Packet Capturing FXML

---
+++ src/ControllerCAContinuousCaptureMain.java
@@ -0,0 +1,90 @@
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ControllerCAContinuousCaptureMain implements Initializable {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private JFXHamburger hamburger;
+
+    @FXML
+    private JFXDrawer drawer;
+
+    @FXML
+    private JFXButton StopBtn;
+
+    @FXML
+    private Text totalPacketsText;
+
+    @FXML
+    private Text totalDroppedText;
+
+    @FXML
+    private Text totalAlertsText;
+
+    @FXML
+    private JFXTextArea loggingTextArea;
+
+    private Scene myScene;
+    public static AnchorPane rootP;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        hamburgerBar();
+    }
+
+    @FXML
+    public void stopCapture(ActionEvent event) {
+
+    }
+
+    public void hamburgerBar() {
+        rootP = anchorPane;
+
+        try {
+            VBox box = FXMLLoader.load(getClass().getResource("SideTab.fxml"));
+            drawer.setSidePane(box);
+            drawer.setVisible(false);
+            drawer.setDefaultDrawerSize(219);
+        } catch (IOException ex) {
+            Logger.getLogger(ControllerBaseLayoutNew.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(hamburger);
+        transition.setRate(-1);
+        hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, (e) -> {
+            transition.setRate(transition.getRate() * -1);
+            transition.play();
+
+            if (drawer.isOpened()) {
+                drawer.close();
+                drawer.setDisable(true);
+                //drawer.setVisible(false);
+            } else {
+                drawer.open();
+                drawer.setVisible(true);
+                drawer.setDisable(false);
+            }
+        });
+    }
+}
