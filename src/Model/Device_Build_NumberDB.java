f25293969fe139f4bb1f6dba90aef9959c04a219
Kool-Koder <39305371+Kool-Koder@users.noreply.github.com>
2018-07-08 16:19:31 +0800

    Finished all 3 methods for Device-Build-Number DB
    Only tested DeleteOSVersion.
    
    If there are bugs in the other 2 methods, please update

--- src/Model/Device_Build_NumberDB.java
+++ src/Model/Device_Build_NumberDB.java
@@ -3,14 +3,14 @@ package Model;
 import java.sql.*;
 import java.util.ArrayList;
 
-public class Device_Build_NumberDB{
-        public ArrayList<OSVersion> CheckSupportedVersion() throws SQLException {
-            ArrayList<OSVersion> OSVersionList=new ArrayList<OSVersion>();
+public class Device_Build_NumberDB {
+    public ArrayList<OSVersion> CheckSupportedVersion() throws SQLException {
+        ArrayList<OSVersion> OSVersionList = new ArrayList<OSVersion>();
 
-            String instanceConnectionName = "netsecpj:us-central1:nspj-project";
-            String databaseName = "device_build_number";
-            String username = "root";
-            String password = "root";
+        String instanceConnectionName = "netsecpj:us-central1:nspj-project";
+        String databaseName = "device_build_number";
+        String username = "root";
+        String password = "root";
 
 //            if (instanceConnectionName.equals("<device-supported-versions>")) {
 //                System.err.println("Please update the sample to specify the instance connection name.");
@@ -22,31 +22,31 @@ public class Device_Build_NumberDB{
 //                System.exit(1);
 //            }
 
-            //[START doc-example]
-            String jdbcUrl = String.format(
-                    "jdbc:mysql://google/%s?cloudSqlInstance=%s"
-                            + "&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false",
-                    databaseName,
-                    instanceConnectionName);
+        //[START doc-example]
+        String jdbcUrl = String.format(
+                "jdbc:mysql://google/%s?cloudSqlInstance=%s"
+                        + "&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false",
+                databaseName,
+                instanceConnectionName);
 
-            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
-            //[END doc-example]
+        Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+        //[END doc-example]
 
-            try (Statement statement = connection.createStatement()) {
+        try (Statement statement = connection.createStatement()) {
 //            ResultSet resultSet = statement.executeQuery("SELECT * FROM entries");
-                ResultSet resultSet = statement.executeQuery("SELECT versionNumber FROM entries");
-                while (resultSet.next()) {
-                    OSVersion osVersion=new OSVersion();
-                    osVersion.setVersionName(resultSet.getString("versionName"));
-                    osVersion.setVersionNumber(resultSet.getString("versionNumber"));
-                    osVersion.setEntryID(resultSet.getInt("entryID"));
+            ResultSet resultSet = statement.executeQuery("SELECT versionNumber FROM entries");
+            while (resultSet.next()) {
+                OSVersion osVersion = new OSVersion();
+                osVersion.setVersionName(resultSet.getString("versionName"));
+                osVersion.setVersionNumber(resultSet.getString("versionNumber"));
+                osVersion.setEntryID(resultSet.getInt("entryID"));
 //                    SupportedVersions.add(resultSet.getString(1));
-                }
             }
-            return  OSVersionList;
         }
+        return OSVersionList;
+    }
 
-    public void insertNewOSVersion(String versionName,String versionNumber) throws SQLException {
+    public void insertNewOSVersion(String versionName, String versionNumber) throws SQLException {
         // TODO: fill this in
         // The instance connection name can be obtained from the instance overview page in Cloud Console
         // or by running "gcloud sql instances describe <instance> | grep connectionName".
@@ -84,52 +84,31 @@ public class Device_Build_NumberDB{
         //[END doc-example]
 
         try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("INSERT INTO entries (versionName, versionNumber) VALUES (\""+versionName+"\"),(\""+versionNumber+"\")");
+            statement.executeUpdate("INSERT INTO entries (versionName, versionNumber) VALUES ('" + versionName + "'),('" + versionNumber + "')");
         }
     }
 
-    public void deleteOSVersion(){
-          //TODO FINISH THIS
+    //TODO FINISH THIS
+    //Tested and Working
+    public void deleteOSVersion(String entryID) throws SQLException {
+        //Test out whether needs to be int or String also can
+        String instanceConnectionName = "netsecpj:us-central1:nspj-project";
+        String databaseName = "device_build_number";
+        String username = "root";
+        String password = "root";
 
+        //[START doc-example]
+        String jdbcUrl = String.format(
+                "jdbc:mysql://google/%s?cloudSqlInstance=%s"
+                        + "&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false",
+                databaseName,
+                instanceConnectionName);
 
-//        // TODO: fill this in
-//        // The instance connection name can be obtained from the instance overview page in Cloud Console
-//        // or by running "gcloud sql instances describe <instance> | grep connectionName".
-//        String instanceConnectionName = "netsecpj:us-central1:nspj-project";
-//
-//        // TODO: fill this in
-//        // The database from which to list tables.
-//        String databaseName = "device_build_number";
-//
-//        String username = "root";
-//
-//        // TODO: fill this in
-//        // This is the password that was set via the Cloud Console or empty if never set
-//        // (not recommended).
-//        String password = "root";
-//
-////            if (instanceConnectionName.equals("<device-supported-versions>")) {
-////                System.err.println("Please update the sample to specify the instance connection name.");
-////                System.exit(1);
-////            }
-////
-////            if (password.equals("<insert_password>")) {
-////                System.err.println("Please update the sample to specify the mysql password.");
-////                System.exit(1);
-////            }
-//
-//        //[START doc-example]
-//        String jdbcUrl = String.format(
-//                "jdbc:mysql://google/%s?cloudSqlInstance=%s"
-//                        + "&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false",
-//                databaseName,
-//                instanceConnectionName);
-//
-//        Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
-//        //[END doc-example]
-//
-//        try (Statement statement = connection.createStatement()) {
-//            statement.executeUpdate("INSERT INTO entries (versionName, versionNumber) VALUES (\""+versionName+"\"),(\""+versionNumber+"\")");
-//        }
+        Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+        //[END doc-example]
+
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate("DELETE FROM entries WHERE entryID=" + entryID);
+        }
     }
 }
