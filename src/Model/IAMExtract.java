f02e4b7abe2b741bd4bcaf5aab50da1a8ea59934
Winston Lim <winstonlim.2000@gmail.com>
2018-07-14 13:36:41 +0800

    Stable IAM Filtering

--- src/Model/IAMExtract.java
+++ src/Model/IAMExtract.java
@@ -1,17 +1,23 @@
 package Model;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class IAMExtract {
     private static String globalRole;
     private static String globalUser;
+    static ArrayList<String> bringiamlist = new ArrayList<>();
 
     public IAMExtract(){
 
     }
 
     public IAMExtract(ArrayList<String> getiamlist){
-        globalRole = getiamlist.get(getiamlist.size());
+        this.bringiamlist=getiamlist;
+        System.out.println("IAM EXTRACT ROLE IS : " + bringiamlist.size());
+        int index = bringiamlist.size()-1;
+        globalRole = bringiamlist.get(index);
+//        globalRole="YOYOYO";
         System.out.println("IAMEXTRACT GLOBALROLE IS : " + globalRole);
         for (int n = 0; n < getiamlist.size() - 1; n++) {
             globalUser = getiamlist.get(n);
