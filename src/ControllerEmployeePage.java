85e71346e331e355663787338ba21153b9850bcd
Winston Lim <winstonlim.2000@gmail.com>
2018-07-16 22:37:18 +0800

    Employee Database Revoke, Creation

--- src/ControllerEmployeePage.java
+++ src/ControllerEmployeePage.java
@@ -1,9 +1,11 @@
 import Database.User;
 import Database.User_InfoDB;
+import Model.ActionButtonTableCell;
 import Model.GetIAM;
 import Model.IAMExtract;
 import Model.IAMPermissions;
-import com.google.cloud.logging.Logging;
+import com.google.api.services.iam.v1.Iam;
+import com.jfoenix.animation.alert.JFXAlertAnimation;
 import com.jfoenix.controls.*;
 import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import javafx.application.Platform;
@@ -15,13 +17,17 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -29,7 +35,6 @@ import java.sql.SQLException;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
 
 public class ControllerEmployeePage implements Initializable {
 
@@ -70,10 +75,16 @@ public class ControllerEmployeePage implements Initializable {
     private TableColumn<User, String> hashpassword;
 
     @FXML
+    private TableColumn<User, Button> deletingEmployees;
+
+    @FXML
     private JFXComboBox<String> jfxcombobox;
 
     @FXML
     private TableView<IAMExtract> rolesTable;
+
+    @FXML
+    private JFXButton createUser;
 
     @FXML
     private TableColumn<IAMExtract, String> roleColumn;
@@ -87,6 +98,15 @@ public class ControllerEmployeePage implements Initializable {
     private Scene myScene;
 
     public static AnchorPane rootP;
+
+    String errorMessage = "";
+    String successfulMessage = "";
+    String doubleConfirm = "";
+    int CHECKING;
+    int checker2;
+    String CreateUser;
+
+    static String email1;
 
     User_InfoDB userinfodb = new User_InfoDB();
     User users = new User();
@@ -140,6 +160,19 @@ public class ControllerEmployeePage implements Initializable {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        deletingEmployees.setCellFactory(ActionButtonTableCell.<User>forTableColumn("Revoke", (User users) -> {
+            user(users);
+            email1 = users.getEmail();
+            System.out.println("THIS IS THE EMAIL!!" + email1);
+
+            doubleConfirm = "This selected user \"" + users.getEmail() + "\" will be removed from the cloud database. Are you sure to delete it?";
+            doubleConfirmation(anchorPane.getScene(), doubleConfirm, "No", "Yes");
+            CHECKING = checker2;
+            System.out.println("CHECKER NOW IS " + CHECKING);
+
+            return users;
+        }));
 
         userObservableList = FXCollections.observableList(userList);
         employeeTable.setItems(userObservableList);
@@ -226,11 +259,10 @@ public class ControllerEmployeePage implements Initializable {
                     }
 
                     Platform.runLater(() -> {
-                        if (globalChecker == 1){
+                        if (globalChecker == 1) {
                             ownerObservableList = FXCollections.observableList(getIAMLists);
                             rolesTable.setItems(ownerObservableList);
-                        }
-                        else if (globalChecker == 2) {
+                        } else if (globalChecker == 2) {
                             editorObservableList = FXCollections.observableList(getIAMLists);
                             rolesTable.setItems(editorObservableList);
                         } else if (globalChecker == 3) {
@@ -272,38 +304,6 @@ public class ControllerEmployeePage implements Initializable {
         chosenRole = jfxcombobox.getSelectionModel().getSelectedItem();
         System.out.println(chosenRole);
         process.start();
-//        if (globalChecker == 1){
-//            ownerObservableList = FXCollections.observableList(getIAMLists);
-//            rolesTable.setItems(ownerObservableList);
-//        }
-//        else if (globalChecker == 2) {
-//            editorObservableList = FXCollections.observableList(getIAMLists);
-//            rolesTable.setItems(editorObservableList);
-//        } else if (globalChecker == 3) {
-//            viewerObservableList = FXCollections.observableList(getIAMLists);
-//            rolesTable.setItems(viewerObservableList);
-//        } else if (globalChecker == 4) {
-//            cloudsqlObservableList = FXCollections.observableList(getIAMLists);
-//            rolesTable.setItems(cloudsqlObservableList);
-//        } else if (globalChecker == 5) {
-//            firebaseObservableList = FXCollections.observableList(getIAMLists);
-//            rolesTable.setItems(firebaseObservableList);
-//        } else if (globalChecker == 6) {
-//            computeengineObservableList = FXCollections.observableList(getIAMLists);
-//            rolesTable.setItems(computeengineObservableList);
-//        } else if (globalChecker == 7) {
-//            loggingadminObservableList = FXCollections.observableList(getIAMLists);
-//            rolesTable.setItems(loggingadminObservableList);
-//        } else if (globalChecker == 8) {
-//            storageadminObservableList = FXCollections.observableList(getIAMLists);
-//            rolesTable.setItems(storageadminObservableList);
-//        } else if (globalChecker == 9) {
-//            monitoringadminObservableList = FXCollections.observableList(getIAMLists);
-//            rolesTable.setItems(monitoringadminObservableList);
-//        } else {
-//            apikeysadminObservableList = FXCollections.observableList(getIAMLists);
-//            rolesTable.setItems(apikeysadminObservableList);
-//        }
 
         process.setOnSucceeded(e -> {
             spinner.setVisible(false);
@@ -317,11 +317,241 @@ public class ControllerEmployeePage implements Initializable {
         });
     }
 
+    Service process1 = new Service() {
+        @Override
+        protected Task createTask() {
+            return new Task() {
+                @Override
+                protected Void call() throws Exception {
+                    try {
+                        userList = userinfodb.getUserList();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+
+                    Platform.runLater(() -> {
+                        userObservableList = FXCollections.observableList(userList);
+                        employeeTable.setItems(userObservableList);
+                    });
+                    return null;
+                }
+
+            };
+        }
+    };
+
 
     @FXML
     void onClickEmployee(MouseEvent event) {
         secondAnchor.setVisible(false);
         employeeTable.setVisible(true);
+//        try {
+//            userList = userinfodb.getUserList();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+
+//        userObservableList = FXCollections.observableList(userList);
+//        employeeTable.setItems(userObservableList);
+        process1.start();
+
+
+        process.setOnSucceeded(e -> {
+            userObservableList = FXCollections.observableList(userList);
+            employeeTable.setItems(userObservableList);
+            process.reset();
+        });
+        process.setOnCancelled(e -> {
+            process.reset();
+        });
+        process.setOnFailed(e -> {
+            process.reset();
+        });
+    }
+
+    @FXML
+    void onClickCreate(MouseEvent event) {
+        creatingUSERS(anchorPane.getScene(),"No", "Yes");
+        CHECKING = checker2;
+        System.out.println("CHECKER NOW IS " + CHECKING);
+    }
+
+    private void doubleConfirmation(Scene scene, String doubleconfirm, String buttonContent, String buttonContent2) {
+        checker2 = -1;
+        myScene = scene;
+        Stage stage = (Stage) (myScene).getWindow();
+
+        String message = doubleconfirm;
+        String title = "Are you sure?";
+
+        JFXButton no = new JFXButton(buttonContent);
+        no.setButtonType(JFXButton.ButtonType.RAISED);
+        no.setStyle("-fx-background-color: #00bfff;");
+
+        JFXButton yes = new JFXButton(buttonContent2);
+        yes.setButtonType(JFXButton.ButtonType.RAISED);
+        yes.setStyle("-fx-background-color: #ff2828");
+
+        JFXDialogLayout layout = new JFXDialogLayout();
+        layout.setHeading(new Label(title));
+        layout.setBody(new Label(message));
+
+        layout.setActions(no, yes);
+
+        JFXAlert<Void> alert = new JFXAlert<>(stage);
+        alert.setOverlayClose(true);
+        alert.setAnimation(JFXAlertAnimation.CENTER_ANIMATION);
+        alert.setContent(layout);
+        alert.initModality(Modality.NONE);
+
+        //GET WHETHER PRESS YES or NO
+        yes.setOnAction(__addEvent -> {
+            checker2 = 1;
+            CHECKING = -1;
+            System.out.println("YES IS PRESSED, CHECKER2 is " + checker2);
+
+            CHECKING = checker2;
+            try {
+                userinfodb.deleteUser(email1);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            employeeTable.getItems().remove(users);
+            successfulMessage = "This user " + email1 +" was successfully removed.";
+            successfulMessage(anchorPane.getScene(), successfulMessage, "Close");
+            alert.hideWithAnimation();
+        });
+        no.setOnAction(__addEvent -> {
+            checker2 = 0;
+            System.out.println("NO IS PRESSED, CHECKER2 is " + checker2);
+            errorMessage = "This user " + email1 + " was not successfully removed.";
+            errorMessagePopOut(anchorPane.getScene(), errorMessage, "Close");
+            alert.hideWithAnimation();
+        });
+        alert.show();
+    }
+
+    private void creatingUSERS(Scene scene, String buttonContent, String buttonContent2) {
+        myScene = scene;
+        Stage stage = (Stage) (myScene).getWindow();
+
+        String message = "Please input the follow required data to create the user.";
+        String title = "Create User" + "\n" + message;
+        JFXButton no = new JFXButton(buttonContent);
+        no.setButtonType(JFXButton.ButtonType.RAISED);
+        no.setStyle("-fx-background-color: #00bfff;");
+
+        JFXButton yes = new JFXButton(buttonContent2);
+        yes.setButtonType(JFXButton.ButtonType.RAISED);
+        yes.setStyle("-fx-background-color: #ff2828");
+
+        String message1 = "Enter email: ";
+
+        JFXDialogLayout layout = new JFXDialogLayout();
+        layout.setPrefSize(546,299);
+        layout.setHeading(new Label(title));
+
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(10, 100, 10, 0));
+
+        JFXTextField inputUser = new JFXTextField();
+        inputUser.setPrefWidth(250);
+        inputUser.setPromptText("Type Email Here");
+
+        grid.add(new Label(message1),0,0);
+        grid.add(inputUser,1,0);
+        layout.setBody(grid);
+
+        layout.setActions(no, yes);
+
+        JFXAlert<Void> alert = new JFXAlert<>(stage);
+        alert.setOverlayClose(true);
+        alert.setAnimation(JFXAlertAnimation.CENTER_ANIMATION);
+        alert.setContent(layout);
+        alert.initModality(Modality.NONE);
+
+        yes.setOnAction(__addEvent -> {
+            checker2 = 1;
+            CHECKING = -1;
+            System.out.println("YES IS PRESSED, CHECKER2 is " + checker2);
+            CHECKING = checker2;
+            CreateUser = inputUser.getText();
+            System.out.println("INPUT NAME IS : " + CreateUser);
+            if(CreateUser.contains("@gmail.com")) {
+                try {
+                    userinfodb.createUser(CreateUser);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                successfulMessage="This user "+CreateUser+ " was created and added into the cloud.";
+                successfulMessage(anchorPane.getScene(),successfulMessage, "Close");
+                alert.hideWithAnimation();
+            }else{
+                System.out.println("NOT ACCEPTED AS NOT A VALID EMAIL");
+                errorMessage="Please try again. Email was not accepted.";
+                errorMessagePopOut(anchorPane.getScene(),errorMessage, "Close");
+            }
+        });
+        no.setOnAction(__addEvent -> {
+            checker2 = 0;
+            System.out.println("NO IS PRESSED, CHECKER2 is " + checker2);
+
+            alert.hideWithAnimation();
+        });
+
+        alert.show();
+    }
+
+    private void errorMessagePopOut(Scene scene, String errorMessage, String buttonContent) {
+        myScene = scene;
+        Stage stage = (Stage) (myScene).getWindow();
+
+        String message = errorMessage;
+        String title = "ERROR";
+        JFXButton close = new JFXButton(buttonContent);
+
+        close.setButtonType(JFXButton.ButtonType.RAISED);
+
+        close.setStyle("-fx-background-color: #00bfff;");
+
+        JFXDialogLayout layout = new JFXDialogLayout();
+        layout.setHeading(new Label(title));
+        layout.setBody(new Label(message));
+        layout.setActions(close);
+        JFXAlert<Void> alert = new JFXAlert<>(stage);
+        alert.setOverlayClose(true);
+        alert.setAnimation(JFXAlertAnimation.CENTER_ANIMATION);
+        alert.setContent(layout);
+        alert.initModality(Modality.NONE);
+        close.setOnAction(__ -> alert.hideWithAnimation());
+        alert.show();
+    }
+
+    private void successfulMessage(Scene scene, String successfulMessage, String buttonContent) {
+        myScene = scene;
+        Stage stage = (Stage) (myScene).getWindow();
+
+        String message = successfulMessage;
+        String title = "Success";
+        JFXButton close = new JFXButton(buttonContent);
+
+        close.setButtonType(JFXButton.ButtonType.RAISED);
+
+        close.setStyle("-fx-background-color: #00bfff;");
+
+        JFXDialogLayout layout = new JFXDialogLayout();
+        layout.setHeading(new Label(title));
+        layout.setBody(new Label(message));
+        layout.setActions(close);
+        JFXAlert<Void> alert = new JFXAlert<>(stage);
+        alert.setOverlayClose(true);
+        alert.setAnimation(JFXAlertAnimation.CENTER_ANIMATION);
+        alert.setContent(layout);
+        alert.initModality(Modality.NONE);
+        close.setOnAction(__ -> alert.hideWithAnimation());
+        alert.show();
     }
 
     public void hamburgerBar() {
