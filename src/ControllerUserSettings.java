d8d27ad485928bcad093b23e8d881da48c8d7ea8
Ravin <170552L@mymail.nyp.edu.sg>
2018-07-18 23:51:12 +0800

    Fixed hamburger in UserSettings

--- src/ControllerUserSettings.java
+++ src/ControllerUserSettings.java
@@ -2,16 +2,22 @@ import Database.User_InfoDB;
 import Model.OAuth2Login;
 import com.google.api.client.auth.oauth2.Credential;
 import com.jfoenix.controls.*;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-
+import javafx.scene.layout.VBox;
 import java.io.IOException;
-import java.sql.SQLException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class ControllerUserSettings {
+public class ControllerUserSettings implements Initializable {
 
 
     @FXML
@@ -37,6 +43,43 @@ public class ControllerUserSettings {
 
     @FXML
     private JFXPasswordField password;
+
+    public static AnchorPane rootP;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        hamburgerBar();
+    }
+
+    public void hamburgerBar() {
+        rootP = anchorPane;
+
+        try {
+            VBox box = FXMLLoader.load(getClass().getResource("UserSideTab.fxml"));
+            drawer.setSidePane(box);
+            drawer.setVisible(false);
+            drawer.setDefaultDrawerSize(219);
+        } catch (IOException ex) {
+            Logger.getLogger(ControllerBaseLayoutNew.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(hamburger);
+        transition.setRate(-1);
+        hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, (e) -> {
+            transition.setRate(transition.getRate() * -1);
+            transition.play();
+
+            if (drawer.isOpened()) {
+                drawer.close();
+                drawer.setDisable(true);
+                //drawer.setVisible(false);
+            } else {
+                drawer.open();
+                drawer.setVisible(true);
+                drawer.setDisable(false);
+            }
+        });
+    }
 
     @FXML
     void newNumber(ActionEvent event) {
