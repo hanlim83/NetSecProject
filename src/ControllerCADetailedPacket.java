cf1969f4a855976f713f83d98103f35ec9571b01
Hansen Lim <hanlim83@users.noreply.github.com>
2018-07-22 22:52:29 +0800

    Done up detailed packet page

--- src/ControllerCADetailedPacket.java
+++ src/ControllerCADetailedPacket.java
@@ -5,19 +5,25 @@ import Model.ScheduledExecutorServiceHandler;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
-import com.jfoenix.controls.JFXTreeTableView;
 import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import org.pcap4j.core.PcapNetworkInterface;
+import org.pcap4j.packet.*;
 
 import java.io.IOException;
 import java.net.URL;
@@ -33,7 +39,9 @@ public class ControllerCADetailedPacket implements Initializable {
     @FXML
     private JFXHamburger hamburger;
     @FXML
-    private JFXTreeTableView<?> packetDetailsTable;
+    private TreeTableView<String> packetDetailsTable;
+    @FXML
+    private TreeTableColumn<String, String> packetDetailsCol;
     @FXML
     private JFXDrawer drawer;
     @FXML
@@ -68,6 +76,7 @@ public class ControllerCADetailedPacket implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        populatePacketDetails();
     }
 
     public void hamburgerBar() {
@@ -100,8 +109,137 @@ public class ControllerCADetailedPacket implements Initializable {
         });
     }
 
+    public void populatePacketDetails() {
+        Packet orignalPacket = packet.getOriginalPacket();
+        TreeItem<String> dummyRoot = new TreeItem<String>();
+        TreeItem<String> PacketDetails = new TreeItem<String>("Packet Details");
+        TreeItem<String> PacketNumber = new TreeItem<String>("Packet Number: " + packet.getNumber());
+        TreeItem<String> CapturedInterface = new TreeItem<String>("Captured on: " + device.getDescription());
+        TreeItem<String> InterfaceID = new TreeItem<String>("Interface ID: " + device.getName());
+        CapturedInterface.getChildren().add(InterfaceID);
+        TreeItem<String> PacketTimeStamp = new TreeItem<String>("TimeStamp: " + packet.getOrignalTimeStamp().toString());
+        TreeItem<String> PacketLength = new TreeItem<String>("Length: " + packet.getLength());
+        PacketDetails.getChildren().add(PacketNumber);
+        PacketDetails.getChildren().add(CapturedInterface);
+        PacketDetails.getChildren().add(PacketTimeStamp);
+        PacketDetails.getChildren().add(PacketLength);
+        dummyRoot.getChildren().add(PacketDetails);
+        if (orignalPacket.contains(IpPacket.class)) {
+            IpPacket ip = orignalPacket.get(IpPacket.class);
+            TreeItem<String> IPHeader = new TreeItem<String>("Internet Protocol");
+            TreeItem<String> IPVersion = new TreeItem<String>("Version: " + ip.getHeader().getVersion());
+            TreeItem<String> IPSrcAddr = new TreeItem<String>("Source Address: " + ip.getHeader().getSrcAddr().getHostAddress());
+            TreeItem<String> IPSDstAddr = new TreeItem<String>("Source Address: " + ip.getHeader().getDstAddr().getHostAddress());
+            TreeItem<String> IPRawData = new TreeItem<String>("Raw Data: " + ip.getHeader().getRawData());
+            IPHeader.getChildren().add(IPVersion);
+            IPHeader.getChildren().add(IPSrcAddr);
+            IPHeader.getChildren().add(IPSDstAddr);
+            IPHeader.getChildren().add(IPRawData);
+            dummyRoot.getChildren().add(IPHeader);
+        } else if (orignalPacket.contains(ArpPacket.class)) {
+            ArpPacket arp = orignalPacket.get(ArpPacket.class);
+            TreeItem<String> ARPHeader = new TreeItem<String>("Address Resolution Protocol");
+            TreeItem<String> ARPType = new TreeItem<String>("Operation: " + arp.getHeader().getOperation().name());
+            TreeItem<String> ARPSrcIP = new TreeItem<String>("Source IP Address: " + arp.getHeader().getSrcProtocolAddr().getHostAddress());
+            TreeItem<String> ARPSrcMac = new TreeItem<String>("Source MAC Address: " + arp.getHeader().getSrcHardwareAddr());
+            TreeItem<String> ARPDstIP = new TreeItem<String>("Destination IP Address: " + arp.getHeader().getDstProtocolAddr().getHostAddress());
+            TreeItem<String> ARPDstMac = new TreeItem<String>("Destination MAC Address: " + arp.getHeader().getDstHardwareAddr());
+            ARPHeader.getChildren().add(ARPType);
+            ARPHeader.getChildren().add(ARPSrcIP);
+            ARPHeader.getChildren().add(ARPSrcMac);
+            ARPHeader.getChildren().add(ARPDstIP);
+            ARPHeader.getChildren().add(ARPDstMac);
+            dummyRoot.getChildren().add(ARPHeader);
+        }
+        if (orignalPacket.contains(TcpPacket.class)) {
+            TcpPacket tcp = orignalPacket.get(TcpPacket.class);
+            TreeItem<String> TCPHeader = new TreeItem<String>("Transmission Control Protocol");
+            TreeItem<String> TCPSrcPort = new TreeItem<String>("Source Port: " + tcp.getHeader().getSrcPort().valueAsString());
+            TreeItem<String> TCPDstPort = new TreeItem<String>("Destination Port: " + tcp.getHeader().getDstPort().valueAsString());
+            TreeItem<String> TCPSequence = new TreeItem<String>("Sequence Number: " + tcp.getHeader().getSequenceNumber());
+            TreeItem<String> TCPAcknowledgement = new TreeItem<String>("Acknowledgement Number: " + tcp.getHeader().getAcknowledgmentNumber());
+            TreeItem<String> TCPFlag;
+            if (tcp.getHeader().getAck())
+                TCPFlag = new TreeItem<String>("Flag: Acknowledgement");
+            else if (tcp.getHeader().getFin())
+                TCPFlag = new TreeItem<String>("Flag: Finished");
+            else if (tcp.getHeader().getPsh())
+                TCPFlag = new TreeItem<String>("Flag: Push");
+            else if (tcp.getHeader().getRst())
+                TCPFlag = new TreeItem<String>("Flag: Reset");
+            else if (tcp.getHeader().getSyn())
+                TCPFlag = new TreeItem<String>("Flag: Synchronisation");
+            else if (tcp.getHeader().getUrg())
+                TCPFlag = new TreeItem<String>("Flag: Urgent");
+            else {
+                TCPFlag = new TreeItem<String>("");
+            }
+            TreeItem<String> TCPChecksum = new TreeItem<String>("Checksum: " + tcp.getHeader().getChecksum());
+            TCPHeader.getChildren().add(TCPSrcPort);
+            TCPHeader.getChildren().add(TCPDstPort);
+            TCPHeader.getChildren().add(TCPSequence);
+            TCPHeader.getChildren().add(TCPAcknowledgement);
+            TCPHeader.getChildren().add(TCPChecksum);
+            TCPHeader.getChildren().add(TCPFlag);
+            dummyRoot.getChildren().add(TCPHeader);
+        } else if (orignalPacket.contains(UdpPacket.class)) {
+            UdpPacket udp = orignalPacket.get(UdpPacket.class);
+            TreeItem<String> UDPHeader = new TreeItem<String>("User Datagram Protocol");
+            TreeItem<String> UDPSrcPort = new TreeItem<String>("Source Port: " + udp.getHeader().getSrcPort());
+            TreeItem<String> UDPDstPort = new TreeItem<String>("Destination Port: " + udp.getHeader().getDstPort());
+            TreeItem<String> UDPLength = new TreeItem<String>("Length: " + udp.getHeader().getLengthAsInt());
+            TreeItem<String> UDPChecksum = new TreeItem<String>("Checksum: " + udp.getHeader().getChecksum());
+            UDPHeader.getChildren().add(UDPSrcPort);
+            UDPHeader.getChildren().add(UDPDstPort);
+            UDPHeader.getChildren().add(UDPLength);
+            UDPHeader.getChildren().add(UDPChecksum);
+            dummyRoot.getChildren().add(UDPHeader);
+        } else if (orignalPacket.contains(IcmpV4CommonPacket.class)) {
+            IcmpV4CommonPacket icmp4 = orignalPacket.get(IcmpV4CommonPacket.class);
+            TreeItem<String> ICMP4Header = new TreeItem<String>("Internet Control Message Protocol v4");
+            TreeItem<String> ICMP4Code = new TreeItem<String>("Code: " + icmp4.getHeader().getCode().name());
+            TreeItem<String> ICMP4Type = new TreeItem<String>("Type: " + icmp4.getHeader().getType().name());
+            TreeItem<String> ICMP4length = new TreeItem<String>("Length: " + icmp4.getHeader().length());
+            TreeItem<String> ICMP4Checksum = new TreeItem<String>("Checksum: " + icmp4.getHeader().getChecksum());
+            ICMP4Header.getChildren().add(ICMP4Code);
+            ICMP4Header.getChildren().add(ICMP4Type);
+            ICMP4Header.getChildren().add(ICMP4length);
+            ICMP4Header.getChildren().add(ICMP4Checksum);
+            dummyRoot.getChildren().add(ICMP4Header);
+        } else if (orignalPacket.contains(IcmpV6CommonPacket.class)) {
+            IcmpV6CommonPacket icmp6 = orignalPacket.get(IcmpV6CommonPacket.class);
+            TreeItem<String> ICMP6Header = new TreeItem<String>("Internet Control Message Protocol v6");
+            TreeItem<String> ICMP6Code = new TreeItem<String>("Code: " + icmp6.getHeader().getCode().name());
+            TreeItem<String> ICMP6Type = new TreeItem<String>("Type: " + icmp6.getHeader().getType().name());
+            TreeItem<String> ICMP6length = new TreeItem<String>("Length: " + icmp6.getHeader().length());
+            TreeItem<String> ICMP6Checksum = new TreeItem<String>("Checksum: " + icmp6.getHeader().getChecksum());
+            ICMP6Header.getChildren().add(ICMP6Code);
+            ICMP6Header.getChildren().add(ICMP6Type);
+            ICMP6Header.getChildren().add(ICMP6length);
+            ICMP6Header.getChildren().add(ICMP6Checksum);
+            dummyRoot.getChildren().add(ICMP6Header);
+        } else if (orignalPacket.contains(EncryptedPacket.class)) {
+            EncryptedPacket encryptedPacket = orignalPacket.get(EncryptedPacket.class);
+            TreeItem<String> EncyptedHeader = new TreeItem<String>("Encrypted Packet");
+            TreeItem<String> EncryptedLength = new TreeItem<String>("Length: " + encryptedPacket.getHeader().length());
+            TreeItem<String> EncryptedRawData = new TreeItem<String>("Raw Data: " + encryptedPacket.getHeader().getRawData());
+            EncyptedHeader.getChildren().add(EncryptedLength);
+            EncyptedHeader.getChildren().add(EncryptedRawData);
+            dummyRoot.getChildren().add(EncyptedHeader);
+        }
+//        TreeItem<String>  = new TreeItem<String>();
+        packetDetailsTable.setRoot(dummyRoot);
+        packetDetailsTable.setShowRoot(false);
+        packetDetailsCol.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<String, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<String, String> param) {
+                return new SimpleStringProperty(param.getValue().getValue());
+            }
+        });
+    }
+
     @FXML
-    void returntoCapture(ActionEvent event) {
+    public void returntoCapture(ActionEvent event) {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("CAMainPackets.fxml"));
         myScene = anchorPane.getScene();
         Stage stage = (Stage) (myScene).getWindow();
