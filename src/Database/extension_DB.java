df4502d66ab4f3ebebfb06393394a894e477e610
Kool-Koder <39305371+Kool-Koder@users.noreply.github.com>
2018-08-05 21:26:40 +0800

    Cleared unused libraries and fixed DB bug

--- src/Database/extension_DB.java
+++ src/Database/extension_DB.java
@@ -4,11 +4,6 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class extension_DB {
-//    private static String instanceConnectionName = "netsecpj:us-central1:nspj-project";
-//    private static String databaseName = "extension_DB";
-//    private static String username = "root";
-//    private static String password = "root";
-
     public ArrayList<String> getExtensionList(){
         ArrayList<String> ExtensionList = new ArrayList<String>();
 
@@ -37,12 +32,13 @@ public class extension_DB {
     }
 
     public void createExtension(String extension){
-
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
 
         try {
+            connection = DataSource.getInstance().getConnection();
+
             preparedStatement=connection.prepareStatement("INSERT INTO entriesExtension (extensions) VALUES (?)");
             preparedStatement.setString(1,extension);
 
